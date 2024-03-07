1f409601ce5eaae6b8e77de84d4daded37a91183
Eric Ihle <zekeotronic@gmail.com>
2024-03-06 22:13:20 -0500

    adds calculator functionality

---
+++ js/Calc.js
@@ -0,0 +1,93 @@
+export default class Calc {
+  constructor(displayElement) {
+    this.leftOperand = 0;
+    this.rightOperand = 0;
+    this.operator = '';
+    this.result = 0;
+    this.memory = '';
+    this.operators = ['+', '-', 'x', '/']
+    this.display = displayElement;
+  }
+  setLeftOperand(value) {
+    this.leftOperand = parseFloat(value);
+    return this;
+  }
+  setRightOperand(value) {
+    this.rightOperand = parseFloat(value);
+    return this;
+  }
+  setOperator(value) {
+    this.operator = value;
+    return this;
+  }
+  add() {
+    this.result = this.leftOperand + this.rightOperand;
+    return this;
+  }
+  subtract() {
+    this.result = this.leftOperand - this.rightOperand;
+    return this;
+  }
+  multiply() {
+    this.result = this.leftOperand * this.rightOperand;
+    return this;
+  }
+  divide() {
+    this.result = this.leftOperand / this.rightOperand || 'Error';
+    return this;
+  }
+  getResult() {
+    return this.result;
+  }
+  getMemory() {
+    return this.memory;
+  }
+  operatorIsSet() {
+    return this.operators.some(op => this.memory.includes(op));
+  }
+  getOperator() {
+    return this.operators.find(op => this.memory.includes(op));
+  }
+  handleInput(value) {
+    if (this.operatorIsSet() && this.operators.includes(value)) {
+      return this;
+    } else {
+      this.memory += value;
+    }
+    this.display.textContent = this.memory;
+  }
+  delete() {
+    this.memory = this.memory.slice(0, -1);
+    this.display.textContent = this.memory;
+  }
+  reset() {
+    this.leftOperand = 0;
+    this.rightOperand = 0;
+    this.operator = '';
+    this.result = 0;
+    this.memory = '';
+    this.display.textContent = this.memory;
+  }
+  evaluate() {
+    this.operator = this.getOperator();
+    this.leftOperand = parseFloat(this.memory.split(this.operator)[0]);
+    this.rightOperand = parseFloat(this.memory.split(this.operator)[1]);
+    if (this.operator === "+") {
+      this.result = this.add().getResult();
+    }
+    if (this.operator === "-") {
+      this.result = this.subtract().getResult();
+    }
+    if (this.operator === "x") {
+      this.result = this.multiply().getResult();
+    }
+    if (this.operator === "/") {
+      this.result = this.divide().getResult();
+    }
+    this.memory = '';
+    this.leftOperand = 0;
+    this.rightOperand = 0;
+    this.operator = '';
+    this.display.textContent = this.result.length <= 12 ? this.result : this.result.toExponential(2);
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
+import Calc from './Calc.js';
+
+const display = document.getElementById('result-value');
+const deleteButton = document.getElementById('btn-delete');
+const resetButton = document.getElementById('btn-reset');
+const equalsButton = document.getElementById('btn-equals');
+
+const calculator = new Calc(display);
+
+const buttons = {
+  one: document.getElementById('btn-1'),
+  two: document.getElementById('btn-2'),
+  three: document.getElementById('btn-3'),
+  four: document.getElementById('btn-4'),
+  five: document.getElementById('btn-5'),
+  six: document.getElementById('btn-6'),
+  seven: document.getElementById('btn-7'),
+  eight: document.getElementById('btn-8'),
+  nine: document.getElementById('btn-9'),
+  zero: document.getElementById('btn-0'),
+  decimal: document.getElementById('btn-decimal'),
+  delete: document.getElementById('btn-delete'),
+  reset: document.getElementById('btn-reset'),
+  plus: document.getElementById('btn-plus'),
+  minus: document.getElementById('btn-minus'),
+  times: document.getElementById('btn-times'),
+  divide: document.getElementById('btn-divide'),
+  equals: document.getElementById('btn-equals'),
+}
+
+const inputButtons = [
+  buttons.one,
+  buttons.two,
+  buttons.three,
+  buttons.four,
+  buttons.five,
+  buttons.six,
+  buttons.seven,
+  buttons.eight,
+  buttons.nine,
+  buttons.zero,
+  buttons.plus,
+  buttons.minus,
+  buttons.times,
+  buttons.divide,
+  buttons.decimal
+]
+
 const theme = document.getElementById('theme-css');
 const chooser = document.getElementById('theme-range');
 function changeTheme() {
@@ -12,3 +60,30 @@ function changeTheme() {
 }
 
 chooser.addEventListener('input', changeTheme);
+
+buttons.seven.addEventListener('click', (e) => {
+  console.log(e.target.getAttribute('value'));
+})
+
+inputButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    calculator.handleInput(e.target.getAttribute('value'));
+    console.log(calculator.memory);
+  });
+});
+
+deleteButton.addEventListener('click', () => {
+  calculator.delete();
+  console.log(calculator.memory);
+})
+
+resetButton.addEventListener('click', () => {
+  calculator.reset();
+  console.log(calculator.memory);
+})
+
+equalsButton.addEventListener('click', () => {
+  calculator.evaluate();
+  console.log(calculator);
+
+});
